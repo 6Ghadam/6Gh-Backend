7a9b9c73eacc1a7cc45572cbd3e0f33063d77a8d
MrWooJ <Hurricanc@gmail.com>
2019-06-20 12:52:36 +0430

    Check Temporary Suspended Status Clients to Make them Ready Again

--- common/models/authentication.js
+++ common/models/authentication.js
@@ -1,5 +1,6 @@
 module.exports = async Authentication => {
 
   rootRequire('common/authentication/checkPendingStatus')(Authentication);
+  rootRequire('common/authentication/checkTemporarySuspndStatus')(Authentication);
 
 };
