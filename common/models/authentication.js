7b0136fd31f79d484482f62154d9c8b5312ad140
MrWooJ <Hurricanc@gmail.com>
2019-06-20 12:54:26 +0430

    Check Verified Status Clients to Make them Ready Again

--- common/models/authentication.js
+++ common/models/authentication.js
@@ -2,5 +2,6 @@ module.exports = async Authentication => {
 
   rootRequire('common/authentication/checkPendingStatus')(Authentication);
   rootRequire('common/authentication/checkTemporarySuspndStatus')(Authentication);
+  rootRequire('common/authentication/checkVerifiedStatus')(Authentication);
 
 };
