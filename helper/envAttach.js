a72ff15a15bfc9c3c8eacd7f9dd417c00cf8098e
MrWooJ <Hurricanc@gmail.com>
2019-06-20 13:47:13 +0430

    Refine the Constants inside the EnvAttach Module

--- helper/envAttach.js
+++ helper/envAttach.js
@@ -1,6 +1,10 @@
 const clientStatus = rootRequire('config/client.status.js');
 const verificationStatus = rootRequire('config/verifiaction.status.js');
 
+const authenticationTryCount = 5;
+const authenticationAttemptsThresholdTime = 60 * 1000;
+const authenticationVerifiedTime = 3 * 24 * 60 * 60 * 1000;
+
 module.exports = server => {
   server.vars = {
     config: {
@@ -8,7 +12,9 @@ module.exports = server => {
       verificationStatus
     },
     const: {
-      authenticationTryCount: 5
+      authenticationTryCount,
+      authenticationAttemptsThresholdTime,
+      authenticationVerifiedTime
     }
   };
 };
