7a61e2c8d4f8b1e011f3afef80816a859c9931ca
MrWooJ <Hurricanc@gmail.com>
2019-06-20 12:48:26 +0430

    Add Verification Status Static Config Model and Embed it to EnvAttach

---
+++ config/verification.status.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ready: 'Ready',
+  pending: 'Pending',
+  verified: 'Verified',
+  suspendedTemporary: 'Suspended-Temporary',
+  suspendedPermanent: 'Suspended-Permanent'
+};
--- helper/envAttach.js
+++ helper/envAttach.js
@@ -1,6 +1,10 @@
+const verificationStatus = rootRequire('config/verifiaction.status.js');
+
 module.exports = server => {
   server.vars = {
-    config: {},
+    config: {
+      verificationStatus
+    },
     const: {}
   };
 };
