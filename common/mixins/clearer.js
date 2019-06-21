9268d80ccd58ec53c3823702fac958fac39e0fac
MrWooJ <Hurricanc@gmail.com>
2019-06-21 17:31:15 +0430

    Implement Logic of Clearer Mixin to Remove Base ACLs from Built-in Models

---
+++ common/mixins/clearer.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+function slugify(name) {
+  name = name.replace(/^[A-Z]+/, s => s.toLowerCase());
+  return name.replace(/[A-Z]/g, s => '-' + s.toLowerCase());
+}
+
+module.exports = Model => {
+  const configFile = 
+    path.join('../models/', slugify(Model.modelName) + '.json');
+  const config = require(configFile);
+
+  if (!config || !config.acls) {
+    console.error(
+      'ClearBaseAcls: Failed to load model config from', configFile);
+    return;
+  }
+
+  Model.settings.acls.length = 0;
+  config.acls.forEach(r => Model.settings.acls.push(r));
+};
