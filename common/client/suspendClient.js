e21d7fe10ceb4bc4b7f2bd716e55b4e963fc653b
MrWooJ <Hurricanc@gmail.com>
2019-06-21 13:44:26 +0430

    Implement Logic to Suspend a Default or Completed Client

--- common/client/suspendClient.js
+++ common/client/suspendClient.js
@@ -6,63 +6,75 @@ let app = rootRequire('server/server');
 
 module.exports = Client => {
   
+  // Load the variables from the application server
   const vars = app.vars;
 
   /**
-	 * this remote method function will suspend a client
-   * based on the provided mobile number.
+	 * @function suspendClient Suspend a client either temporary or permanantly
+   * @param {String} clientId String of user's identifier
+	 * @param {Boolean} isPermanantly Flag of suspension level
+   * @returns {Object} Returns the suspended client in case of successful operation
+   * @throws {Error} Throws error if anything happened
 	 */
-  Client.suspendClient = async mobileNumber => {
-    let {
-      authentication,
-      AccessToken
-    } = Client.app.models;
-    // fetch list of all authentication models by the provided mobileNumber.
-    let model = await authentication.findOne({
-      where: {
-        mobileNumber: mobileNumber.toString()
-      }
+  Client.suspendClient = async (clientId, isPermanantly) => {
+    // Fetch the client model based on the provided clientId
+    let clientModel = await Client.fetchModel(clientId.toString());
+    // Suspension will work only for non-guest clients
+    if (clientModel.type === vars.config.clientType.guest) {
+      throw createError(403);
+    }
+    // Find the suspension status based on the isPermanantly flag
+    let suspensionStatus = vars.config.suspensionStatus.permanantSuspended;
+    if (!isPermanantly) {
+      suspensionStatus = vars.config.suspensionStatus.temporarySuspended;
+    }
+    // update client status to permanent suspend
+    clientModel = await clientModel.updateAttribute({
+      isSuspended: suspensionStatus
     });
-    if (!model) { throw createError(404); }
-    // update client authentication status to permanent suspend.
-    await model.updateAttribute({
-      status: vars.config.verificationStatus.suspendedPermanent
-    });
-    // fetch a corresponding client based on the provided mobile number.
-    let fetchedClient = await Client.fetchedClient(mobileNumber);
-    if (!fetchedClient) { throw createError(404); }
-    // remove all related access token of that particular client.
+    // Load AccessToken model
+    let AccessToken = Client.app.models;
+    // Remove all related access token of that particular client.
     await AccessToken.destroyAll({
       where: {
-          ClientId: fetchedClient.id.toString()
+          userId: clientModel.id.toString()
         }
       }
     );
-    return { status: vars.config.verificationStatus.suspendedPermanent };
+    return clientModel;
   };
 
-  Client.suspendClient = utility.wrapper(Client.suspendClient);
+  // Wrapp the function inside the Try/Catch
+  Client.suspendClient = 
+    utility.wrapper(Client.suspendClient);
 
 	/**
-	 * remote method signiture for suspending a client
-   * based on the provided mobile number.
+	 * remote method signiture for permanantly suspending a client
+   * based on the provided clientId and isPermanantly flag.
 	 */
   Client.remoteMethod('suspendClient', {
     description:
-      'suspend a client from accessing to endpoints with its mobile number',
-    accepts: [
-      {
-        arg: 'mobileNumber',
-        type: 'string',
-        required: true,
-        http: {
-          source: 'path'
-        }
+      'Suspend a client from accessing \
+      to endpoints with its clientId',
+    accepts: [{
+      arg: 'clientId',
+      type: 'string',
+      required: true,
+      http: {
+        source: 'path'
       }
-    ],
+    },
+    {
+      arg: 'isPermanantly',
+      type: 'boolean',
+      required: true,
+      http: {
+        source: 'query'
+      }
+    }],
     http: {
-      path: '/suspendClient/:mobileNumber',
-      verb: 'POST',
+      path: '/:clientId/suspendClient',
+      verb: 'PUT',
       status: 200,
       errorStatus: 400
     },
