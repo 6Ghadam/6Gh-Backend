7868ec9c9e12c6609386e58c95d0296835ecd7aa
MrWooJ <Hurricanc@gmail.com>
2019-06-21 13:50:58 +0430

    Upgrade a Client with clientId and Provided Data

--- common/client/upgradeClient.js
+++ common/client/upgradeClient.js
@@ -1,59 +1,56 @@
-const createError = require('http-errors');
-
 const utility = rootRequire('helper/utility');
 
 let app = rootRequire('server/server');
 
 module.exports = Client => {
 
+  // Load the variables from the application server
 	const vars = app.vars;
 
   /**
-	 * @func	 Function - update a client
-	 * @param  {Object} clientId - {client string identifier to update}
-	 * @param  {Object} data - {not set yet!}
+	 * @function upgradeClient Update a client to a higher level
+   * @param {String} clientId String of user's identifier
+	 * @param {Object} data data for upgrading user's profile
+   * @returns {Object} Returns the updated client in case of successful operation
+   * @throws {Error} Throws error if anything happened
 	 */
 	Client.upgradeClient = async (clientId, data) => {
-		// fetch a client based on the provided client identifier.
-		let fetchedClient = await Client.findById(clientId);
-		// send 404 Http status code if there was no client model.
-		if (!fetchedClient) { throw createError(404); }
-		// attach completed status to input data to say it is completed not.
-		data.status = vars.config.clientStatus.completed;
-		// update fetched client by the provided data and return successful object to caller.
-		let updatedClient = await fetchedClient.updateAttributes(data);
-		if (!updatedClient) { throw createError(404); }
-		return updatedClient;
+    // Fetch the client model based on the provided clientId
+    let clientModel = await Client.fetchModel(clientId.toString());
+    // Prepare the data for the updating the client model
+    data.type = vars.config.clientType.completed;
+    // update the client model based on the provided data
+    clientModel = await clientModel.updateAttributes(data);
+    return clientModel;
 	};
 
-	Client.upgradeClient = utility.wrapper(Client.upgradeClient);
+  // Wrapp the function inside the Try/Catch
+  Client.upgradeClient = 
+    utility.wrapper(Client.upgradeClient);
 
 	/**
 	 * remote method signiture for upgrade a client model
    * with provided data
 	 */
   Client.remoteMethod('upgradeClient', {
-    description:
-      'upgrate a particular client with provided data',
-    accepts: [
-      {
-        arg: 'clientId',
-        type: 'string',
-        required: true,
-        http: {
-          source: 'path'
-        }
-			},
-      {
-        arg: 'data',
-        type: 'object',
-        http: {
-          source: 'body'
-        }
+    description: 
+      'Upgrate a particular client with provided data',
+    accepts: [{
+      arg: 'clientId',
+      type: 'string',
+      required: true,
+      http: {
+        source: 'path'
       }
-    ],
+    }, {
+      arg: 'data',
+      type: 'object',
+      http: {
+        source: 'body'
+      }
+    }],
     http: {
-      path: '/upgradeClient/:clientId',
+      path: '/:clientId/upgradeClient',
       verb: 'PUT',
       status: 200,
       errorStatus: 400
