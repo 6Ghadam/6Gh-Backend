67c00270d85a58502540400e66222e863f708a04
MrWooJ <Hurricanc@gmail.com>
2019-06-21 17:29:28 +0430

    Add NodeEnvironment Config File and Change SMS API Token and Fix Typo in DomainName

--- helper/envAttach.js
+++ helper/envAttach.js
@@ -1,21 +1,23 @@
 const clientType = rootRequire('config/client.type.js');
+const nodeEnvironment = rootRequire('config/node.environment.js');
 const suspensionStatus = rootRequire('config/suspension.status.js');
-const verificationStatus = rootRequire('config/verifiaction.status.js');
+const verificationStatus = rootRequire('config/verification.status.js');
 
 const userRealm = 'User';
-const domainName = '6ghadam.com';
+const domainName = '@6ghadam.com';
 const authenticationTryCount = 5;
 const authenticationAttemptsThresholdTime = 60 * 1000;
 const authenticationVerifiedTime = 3 * 24 * 60 * 60 * 1000;
 const smsAPIBaseURL = 'https://api.kavenegar.com/v1/@/verify/lookup.json';
 const smsAPIToken = 
-  '33596B704E5678656A333150565A455A56645A6B55574A47723331797453492F';
+  '33434D58303256744D72316F674A54755734616176413D3D';
 const authenticationTemplate = 'VerificationNo1';
 
 module.exports = server => {
   server.vars = {
     config: {
       clientType,
+      nodeEnvironment,
       suspensionStatus,
       verificationStatus
     },
