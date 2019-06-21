7f7580612754bc5ae7f647c3fa21b5e8d00c5624
MrWooJ <Hurricanc@gmail.com>
2019-06-21 13:23:03 +0430

    Update Configurations and Add New Constants to EnvAttach Module

--- helper/envAttach.js
+++ helper/envAttach.js
@@ -1,20 +1,33 @@
-const clientStatus = rootRequire('config/client.status.js');
+const clientType = rootRequire('config/client.type.js');
+const suspensionStatus = rootRequire('config/suspension.status.js');
 const verificationStatus = rootRequire('config/verifiaction.status.js');
 
+const userRealm = 'User';
+const domainName = '6ghadam.com';
 const authenticationTryCount = 5;
 const authenticationAttemptsThresholdTime = 60 * 1000;
 const authenticationVerifiedTime = 3 * 24 * 60 * 60 * 1000;
+const smsAPIBaseURL = 'https://api.kavenegar.com/v1/@/verify/lookup.json';
+const smsAPIToken = 
+  '33596B704E5678656A333150565A455A56645A6B55574A47723331797453492F';
+const authenticationTemplate = 'VerificationNo1';
 
 module.exports = server => {
   server.vars = {
     config: {
-      clientStatus,
+      clientType,
+      suspensionStatus,
       verificationStatus
     },
     const: {
+      userRealm,
+      domainName,
       authenticationTryCount,
       authenticationAttemptsThresholdTime,
-      authenticationVerifiedTime
+      authenticationVerifiedTime,
+      smsAPIBaseURL,
+      smsAPIToken,
+      authenticationTemplate
     }
   };
 };
