d7416a5f1e157b0548cf10f974785ba2066b8c30
MrWooJ <Hurricanc@gmail.com>
2019-06-23 17:36:05 +0430

    Attach Profile Modules to Profile Model

--- common/models/profile.js
+++ common/models/profile.js
@@ -1,3 +1,14 @@
-module.exports = async () => {
+module.exports = async Profile => {
 
+  rootRequire('common/profile/updateBill')(
+    Profile);
+  rootRequire('common/profile/updateCoin')(
+    Profile);
+  rootRequire('common/profile/updateDurationalScore')(
+    Profile);
+  rootRequire('common/profile/updateScoreLevel')(
+    Profile);
+  rootRequire('common/profile/updateTotalScores')(
+    Profile);
+  
 };
